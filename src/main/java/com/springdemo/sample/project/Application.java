91729ac1a060e33246a18064271bb2f752523373
Omerson Cruz <omerson.e.cruz@gmail.com>
2021-04-03 11:14:23 +0800

    Defining and consuming app.properties

--- src/main/java/com/springdemo/sample/project/Application.java
+++ src/main/java/com/springdemo/sample/project/Application.java
@@ -1,8 +1,8 @@
 package com.springdemo.sample.project;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
